06c22a97d001330da7e3717a512c03060b1c560b
abu-bakarr <abangura14@yahoo.com>
2021-09-21 08:58:21 +0000

    exercise adv

--- monty.h
+++ monty.h
@@ -55,6 +55,7 @@ void op_nop(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
 void op_sub(stack_t **stack, unsigned int line_number);
 void op_div(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
+void op_mod(stack_t **stack, unsigned int line_number);
 
 #endif
